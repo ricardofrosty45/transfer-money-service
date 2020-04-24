eb7f92cee722ac5e3c95ad212b71b6fd487520c3
Luan Ricardo De Luna <luan.venancio@fastalelo.onmicrosoft.com>
2020-04-23 23:19:36 -0300

    First Commit

---
+++ test/src/main/java/br/com/transfer/money/service/main/HelloWorld.java
@@ -0,0 +1,10 @@
+package br.com.transfer.money.service.main;
+
+import java.util.logging.Logger;
+
+public class HelloWorld {
+
+    public static void main(String... args) {
+        Logger logger=Logger.getLogger(HelloWorld.class.getName());
+        logger.info("This is a Hello World!");    }
+}
